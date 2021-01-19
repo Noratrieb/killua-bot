d08e02c488d78f75c58c4bddb220b6191129d7a6
shinixsensei <crezoxplays@gmail.com>
2021-01-19 11:46:15 +0100

    fixed main bs

--- src/main/java/com/github/nilstrieb/core/Main.java
+++ src/main/java/com/github/nilstrieb/core/Main.java
@@ -3,7 +3,6 @@ package com.github.nilstrieb.core;
 import com.github.nilstrieb.cofig.Secrets;
 import com.github.nilstrieb.commands.info.EvalCommand;
 import com.github.nilstrieb.commands.info.HelpCommand;
-import com.github.nilstrieb.listener.CommandListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -12,34 +11,19 @@ import net.dv8tion.jda.api.utils.Compression;
 import javax.security.auth.login.LoginException;
 
 public class Main {
-    public static void main(String[] args) {
-        new Main();
+
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault(Secrets.TOKEN);
+        builder.setCompression(Compression.ZLIB);
+        builder.setActivity(Activity.watching(" over Gon"));
+
+        builder.addEventListeners(builder);
+
+        JDA jda = builder.build();
+        setupCommands();
     }
 
-    public Main() {
-        try {
-            JDABuilder builder = JDABuilder.createDefault(Secrets.TOKEN);
-            builder.setCompression(Compression.ZLIB);
-            builder.setActivity(Activity.watching(" over Gon"));
-
-            addlisteners(builder);
-
-            JDA jda = builder.build();
-            setupCommands();
-            jda.awaitReady();
-
-            System.out.println("started");
-
-        } catch (LoginException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void addlisteners(JDABuilder builder){
-        builder.addEventListeners(new CommandListener());
-    }
-
-    private void setupCommands(){
+    private static void setupCommands() {
         new HelpCommand();
         new EvalCommand();
     }
---
+++ src/main/java/com/github/nilstrieb/listener/StartUpListener.java
@@ -0,0 +1,13 @@
+package com.github.nilstrieb.listener;
+
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class StartUpListener extends ListenerAdapter {
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        System.out.println("started");
+    }
+}
