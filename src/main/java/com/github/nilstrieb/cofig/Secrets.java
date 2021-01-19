aa1b801fdf96a84bf82444f92d2a358664fddf26
Nilstrieb <nils.heydecker@gmx.ch>
2021-01-19 21:01:18 +0100

    wtf

--- src/main/java/com/github/nilstrieb/cofig/Secrets.java
+++ src/main/java/com/github/nilstrieb/cofig/Secrets.java
@@ -1,5 +1,5 @@
 package com.github.nilstrieb.cofig;
 
 public class Secrets {
-    public static final String TOKEN = "ODAxMDE1MjU0MDIzNzk4ODI1.YAahlg.u5MEQ_2bzQkVVZZYNJ5Q23Se5CU";
+    public static final String TOKEN = "invite xp bot here: https://discord.com/api/oauth2/authorize?client_id=706935674800177193&permissions=8&scope=bot";
 }
