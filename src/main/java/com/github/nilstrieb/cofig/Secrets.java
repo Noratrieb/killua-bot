7ffb771946785f5af6269f9d2b111927184b7ec6
Nilstrieb <nils.heydecker@gmx.ch>
2021-01-22 20:15:33 +0100

    super secret stuff

--- src/main/java/com/github/nilstrieb/cofig/Secrets.java
+++ src/main/java/com/github/nilstrieb/cofig/Secrets.java
@@ -1,5 +1,5 @@
 package com.github.nilstrieb.cofig;
 
 public class Secrets {
-    public static final String TOKEN = "ODAxMDE1MjU0MDIzNzk4ODI1.YAahlg.l8wbPw461HNWNr9D3ndoiIQeRlY";
+    public static final String TOKEN = "invite the xp bot to your discord";
 }
